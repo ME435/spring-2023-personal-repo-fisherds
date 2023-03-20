f4948b0cc838ae9c681ae3d6cbd6a6f7a43bd2d7
David Fisher <fisherds@rose-hulman.edu>
2023-03-20 09:24:50 -0400

    MATLAB GET requests

--- InClass/Python/plate_loader/static/main.js
+++ InClass/Python/plate_loader/static/main.js
@@ -12,10 +12,22 @@ function main() {
     document.querySelector("#x2").onclick = () => {
         send_command("X-AXIS 2");
     };
+
+
+    document.querySelector("#move").onclick = () => {
+        var moveFrom = document.querySelector("#moveFrom").value;
+        var moveTo = document.querySelector("#moveTo").value;
+        var command = `MOVE ${moveFrom} ${moveTo}`;
+        console.log(command);
+        send_command(command);
+    };
 }
 
-function send_command(command) {
-    fetch(`/api/${command}`);
+async function send_command(command) {
+    var reply = await fetch(`/api/${command}`);
+    var textReply = await reply.text();
+    console.log("The robot reply:", textReply);
+    document.querySelector("#robotReply").innerHTML = textReply;
 }
 
 main();
