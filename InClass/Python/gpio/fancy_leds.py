6e78f99dd323b8a90f9db6086dae1f644682dc61
Dave Fisher <fisherds@rose-hulman.edu>
2023-04-18 09:37:02 -0400

    fancy LEDs

--- InClass/Python/gpio/fancy_leds.py
+++ InClass/Python/gpio/fancy_leds.py
@@ -5,7 +5,9 @@ import signal
 def main():
     print("Fancy LEDs")
     # fancy_blink()
-    led_pwm()
+    # led_pwm()
+    # led_board()
+    traffic_light_api()
 
 
 def fancy_blink():
@@ -29,7 +31,7 @@ def fancy_blink():
 
 
 def led_pwm():
-    print("Fancy blink")
+    print("LED PWM")
     red_led = gz.PWMLED(14)
     yellow_led = gz.PWMLED(15)
     green_led = gz.PWMLED(18)
@@ -38,5 +40,37 @@ def led_pwm():
         for k in range(100):
             red_led.value = k / 200
             time.sleep(0.01)
+
+
+
+def led_board():
+    print("LED Board")
+    leds = gz.LEDBoard(14, 15, 18, pwm=True)
+
+    leds.on()
+    time.sleep(1)
+    
+    leds.off()
+    time.sleep(1)
+    
+    leds.value = (1, 0, 1)
+    time.sleep(1)
+    
+    leds.value = (0.2, 0.2, 0.2)
+    time.sleep(1)
+    
+    leds.blink()
+    signal.pause()
+
+
+def traffic_light_api():
+    print("TrafficLight api")
+    # Create a TrafficLights object
+
+    # Loop forever
+    #   Green LED on only for 4 seconds (using TrafficLights object)
+    #   Yellow LED on only for 1 seconds
+    #   Red LED on only for 3 seconds
+
 
 main()
