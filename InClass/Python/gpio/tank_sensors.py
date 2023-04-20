dffbadeb91b05f9c1b07553294f3ad836210d2b3
Dave Fisher <fisherds@rose-hulman.edu>
2023-04-20 09:37:48 -0400

    tank line sensors

---
+++ InClass/Python/gpio/tank_sensors.py
@@ -0,0 +1,41 @@
+import gpiozero as gz
+import time
+import signal
+
+
+def main():
+    print("Tank Sensors")
+    # line_sensor_events()
+    line_sensor_states()
+
+
+def line_sensor_states():
+    print("Line Sensor States")
+    starboard_line_sensor = gz.LineSensor(20)
+    port_line_sensor = gz.LineSensor(19)
+    middle_line_sensor = gz.LineSensor(16)
+    
+    while True:
+        print(f"Starboard: {starboard_line_sensor.value} Middle: {middle_line_sensor.value}  Port: {port_line_sensor.value}")
+        # Black is a value of 1
+        # White is a value of 0
+        time.sleep(1.0)
+
+def line_sensor_events():
+    print("Line Sensor Events")
+    starboard_line_sensor = gz.LineSensor(20)
+    port_line_sensor = gz.LineSensor(19)
+    middle_line_sensor = gz.LineSensor(16)
+
+    starboard_line_sensor.when_line = lambda: print("Starboard: white!")
+    starboard_line_sensor.when_no_line = lambda: print("Starboard: black!")
+    
+    middle_line_sensor.when_line = lambda: print("Middle: white!")
+    middle_line_sensor.when_no_line = lambda: print("Middle: black!")
+    
+    port_line_sensor.when_line = lambda: print("Port: white!")
+    port_line_sensor.when_no_line = lambda: print("Port: black!")
+    
+    signal.pause()
+
+main()
