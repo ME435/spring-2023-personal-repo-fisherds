06ac2d684a459b5b578e137da236f57549495927
David Fisher <fisherds@rose-hulman.edu>
2023-04-24 09:33:49 -0400

    matlab simple mqtt

---
+++ InClass/MATLAB/MQTT-Stuff/simpleMqttTest.m
@@ -0,0 +1,25 @@
+function simpleMqttTest()
+clc
+clear all
+% mqClient = mqtt("tcp://broker.hivemq.com")
+mqClient = mqttclient("tcp://broker.hivemq.com");
+subscribe(mqClient, "my_messages", "Callback", @myCallback)
+for k = 1:5
+    % publish()
+    message = sprintf('{"type": "chat", "payload": "fisherds %d"}', k);
+    write(mqClient, "my_messages", message)
+    pause(2)
+end
+
+% unsubscribeAll(mqClient)
+unsubscribe(mqClient)
+end
+
+function myCallback(topic, message)
+
+messageCell = jsondecode(message);
+messageType = messageCell.type;
+messagePayload = messageCell.payload;
+
+fprintf(messagePayload)
+end
