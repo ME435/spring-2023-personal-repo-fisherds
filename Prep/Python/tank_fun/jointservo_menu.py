7662dd1e8604ad9c9f09835d7120875aaa90e787
Dave Fisher <fisherds@rose-hulman.edu>
2023-04-30 21:30:02 -0400

    improving joint menu

--- Prep/Python/tank_fun/jointservo_menu.py
+++ Prep/Python/tank_fun/jointservo_menu.py
@@ -23,35 +23,35 @@ def main():
     while True:
 
         print("Type in a servo number.  Options:")
-        print("11 --> Camera Tilt")
-        print("12 --> Arm Joint 1")
-        print("13 --> Arm Joint 2")
-        print("14 --> Arm Joint 3")
-        print("15 --> Gripper")
-        servo_number = int(input("Servo number (11 to 15) or (0 to exit): "))
-        if servo_number == 0:
+        print("1 --> Arm Joint 1")
+        print("2 --> Arm Joint 2")
+        print("3 --> Arm Joint 3")
+        print("G --> Gripper")
+        print("C --> Camera Tilt")
+        selection = input("Selection: ")
+        if selection == "":
             break
-        elif servo_number == 11:
+        elif selection == "1":
+            angle = int(input("Joint 1 angle: "))
+            angle = interp(angle, [90, -90], [0, 180])  # flip
+            servo_kit.servo[PIN_JOINT_1].angle = angle
+        elif selection == "2":
+            angle = int(input("Joint 2 angle: "))
+            angle = interp(angle, [-90, 90], [0, 180])  # add 90
+            servo_kit.servo[PIN_JOINT_2].angle = angle
+        elif selection == "3":
+            angle = int(input("Joint 3 angle: "))
+            angle = interp(angle, [-90, 90], [0, 180])  # add 90
+            servo_kit.servo[PIN_JOINT_3].angle = angle
+        elif selection == "G":
+            inches = float(input("Gripper distance (inches 0.0 to 2.0): "))
+            angle = interp(inches, [0, 2], [100, 20])
+            print("Request:", inches, " Result: ", angle)
+            servo_kit.servo[PIN_GRIPPER_SERVO].angle = angle
+        elif selection == "C":
             angle = int(input("Camera title angle (0 to 60): "))
-            servo_angle = interp(angle,[0, 45], [30, 0]) # maps 0->45 to 30->0 (flip and scale)
-            servo_kit.servo[PIN_CAMERA_SERVO].angle = servo_angle
-        elif servo_number == PIN_JOINT_1:
-            angle = int(input("Joint 1 angle (-90 to 90): "))
-            servo_angle = interp(angle, [-90, 90], [180, 0])  # flip
-            servo_kit.servo[PIN_JOINT_1].angle = servo_angle
-        elif servo_number == PIN_JOINT_2:
-            angle = int(input("Joint 2 angle (-90 to 90): "))
-            servo_angle = interp(angle, [-90, 90], [0, 180])  # add 90
-            servo_kit.servo[PIN_JOINT_2].angle = servo_angle
-        elif servo_number == PIN_JOINT_3:
-            angle = int(input("Joint 3 angle (-90 to 90): "))
-            servo_angle = interp(angle, [-90, 90], [0, 180])  # add 90
-            servo_kit.servo[PIN_JOINT_3].angle = servo_angle
-        elif servo_number == 15:
-            distance_inches = float(input("Gripper distance (inches 0.0 to 2.0): "))
-            servo_angle = interp(inches, [0, 2], [105, 0])
-            print("Request:", inches, " Result: ", servo_angle)
-            servo_kit.servo[PIN_GRIPPER_SERVO].angle = servo_angle
+            angle = interp(angle,[0, 45], [30, 0]) # maps 0->45 to 30->0 (flip and scale)
+            servo_kit.servo[PIN_CAMERA_SERVO].angle = angle
         else:
             print("Invalid servo number")
 
