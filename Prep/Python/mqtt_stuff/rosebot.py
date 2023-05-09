6b49cbba702a422402c535a74241cd5ab180c5cc
David Fisher <fisherds@rose-hulman.edu>
2023-05-08 21:25:32 -0400

    updates to the prep tank gui

--- Prep/Python/mqtt_stuff/rosebot.py
+++ Prep/Python/mqtt_stuff/rosebot.py
@@ -3,35 +3,60 @@ from numpy import interp
 import gpiozero as gz
 import time
 
+
 class RoseBot:
   def __init__(self):
+    self.mode = ""
     self.drive_system = DriveSystem()
     self.arm_servos = ArmServos()
     self.ultrasonic = UltrasonicSensor()
     self.line_sensors = LineSensors()
-
-class Motor:
-  def __init__(self, pin_1, pin_2, pin_enable):
-    self.digital_output_1 = gz.DigitalOutputDevice(pin_1)
-    self.digital_output_2 = gz.DigitalOutputDevice(pin_2)
-    self.pwm_output = gz.PWMOutputDevice(pin_enable,frequency=1000)
-
-  def turn_on(self, duty_cycle):
-    if duty_cycle > 0:
-      self.digital_output_1.on()
-      self.digital_output_2.off()
-      self.pwm_output.value = duty_cycle / 100.0
-    elif duty_cycle < 0:
-      self.digital_output_1.off()
-      self.digital_output_2.on()
-      self.pwm_output.value = -duty_cycle / 100.0
-    else:
-      self.turn_off()
-
-  def turn_off(self):
-    self.digital_output_1.off()
-    self.digital_output_2.off()
-    self.pwm_output.value = 0
+  
+  def set_mode(self, mode):
+    self.mode = mode
+    if self.mode == "go_until_wall" or "go_until_line":
+      self.drive_system.go_forward()    
+    if self.mode == "":
+      self.drive_system.stop()
+     
+  def update_mode(self):
+    if self.mode == "go_until_wall":
+      if self.ultrasonic.get_distance() < 20.0:
+        self.drive_system.stop()
+        self.mode = ""
+    
+    if self.mode == "go_until_line":
+      for reading in self.line_sensors.get_values():
+        if reading == LineSensors.BLACK:
+          self.drive_system.stop()
+          self.mode = ""
+    
+    if self.mode == "line_follow":
+      self.do_line_follow()    
+    
+  def do_line_follow(self):
+    line_sensor_readings = self.line_sensors.get_values()
+    # Algorithm from Evan Cruise
+    if line_sensor_readings == [0, 1, 0]:
+        # in middle: both motors forward
+        print("Go forwards")
+        self.drive_system.go_forward(0.5)
+    elif line_sensor_readings == [1, 1, 0]:
+        # turn slightly left: left motor reverse slow, keep right motor on at high speed
+        print("Slight left turn")
+        self.drive_system.turn_left(0.25, 0.5)
+    elif line_sensor_readings == [1, 0, 0]:
+        # turn hard left: left motor reverse high, keep right motor on at high speed
+        print("Sharp left turn")
+        self.drive_system.turn_left(0.5, 0.5)
+    elif line_sensor_readings == [0, 1, 1]:
+        # turn slightly right: right motor reverse slow, keep left motor on at high speed
+        print("Slight right turn")
+        self.drive_system.turn_right(0.5, 0.25)
+    elif line_sensor_readings == [0, 0, 1]:
+        # turn hard right: right motor reverse high, keep left motor on at high speed
+        print("Sharp right turn")
+        self.drive_system.turn_right(0.5, 0.5)
 
 
 class DriveSystem:
@@ -45,6 +70,38 @@ class DriveSystem:
     self.left_motor = gz.Motor(forward=Motor_B_Pin1, backward=Motor_B_Pin2, enable=Motor_B_EN)
     self.right_motor =gz.Motor(forward=Motor_A_Pin2, backward=Motor_A_Pin1, enable=Motor_A_EN)
 
+  def stop(self):
+     self.left_motor.stop()
+     self.right_motor.stop()
+
+  def drive(self, message_payload):
+     self.drive_motor(self.left_motor, message_payload[0], message_payload[1])
+     self.drive_motor(self.right_motor, message_payload[2], message_payload[3])
+
+  def go_forward(self, pwm=0.6):
+     self.left_motor.forward(pwm)
+     self.right_motor.forward(pwm)
+  
+  def go_backward(self, pwm=0.6):
+     self.left_motor.reverse(pwm)
+     self.right_motor.reverse(pwm)
+  
+  def turn_right(self, leftMotorSpeed, rightMotorSpeed):
+     self.left_motor.forward(leftMotorSpeed)
+     self.right_motor.reverse(rightMotorSpeed)
+
+  def turn_left(self, leftMotorSpeed, rightMotorSpeed):
+     self.left_motor.reverse(leftMotorSpeed)
+     self.right_motor.forward(rightMotorSpeed)
+  
+  def drive_motor(self, motor: gz.Motor, direction, pwm):
+    if direction == 0:
+      motor.stop()
+    elif direction == 1:
+       motor.forward(pwm)
+    elif direction == -1:
+       motor.reverse(pwm)
+        
 
 class ArmServos:
   PIN_JOINT_1 = 12
@@ -55,15 +112,20 @@ class ArmServos:
   def __init__(self):
     self.kit = adafruit_servokit.ServoKit(channels=16)
   
+  def move_joints(self, angles):
+     self.move_joints(1, angles[0])
+     self.move_joints(2, angles[1])
+     self.move_joints(3, angles[2])
+     
   def set_joint_angle(self, joint_number, angle):
-    pin = joint_number + ArmServos.PIN_CAMERA_SERVO  # 12, 13, or 14
+    pin = joint_number + 11  # Little hack to get --> 12, 13, or 14
     if joint_number == 1:
       servo_angle = interp(angle, [-90, 90], [180, 0])  # flip
     else:
       servo_angle = interp(angle, [-90, 90], [0, 180])  # add 90
     self.kit.servo[pin].angle = servo_angle
   
-  def set_gripper_inches(self, inches):
+  def move_gripper(self, inches):
     servo_angle = interp(inches, [0, 2], [105, 0])
     print("Request:", inches, " Result: ", servo_angle)
     self.kit.servo[ArmServos.PIN_GRIPPER_SERVO].angle = servo_angle
@@ -85,13 +147,19 @@ class UltrasonicSensor:
 
 
 class LineSensors:
+    BLACK = 0
+    WHITE = 1
+
     def __init__(self):
         self.left_line = gz.LineSensor(20)
         self.middle_line = gz.LineSensor(16)
         self.right_line = gz.LineSensor(19)
 
+    def get_values(self):
+       return [self.left_line.value, self.middle_line.value, self.right_line.value]
+
     def get_left_value(self):
-        return self.left_line.value
+        return self.left_line.value  # I think maybe 1 is white???
 
     def get_middle_value(self):
         return self.middle_line.value
--- Prep/Python/mqtt_stuff/tank_gui.py
+++ Prep/Python/mqtt_stuff/tank_gui.py
@@ -2,6 +2,7 @@ import mqtt_helper as mh
 import rosebot
 import time
 
+
 class App:
 
     def __init__(self):
@@ -10,8 +11,6 @@ class App:
         self.mqtt_client = mh.MqttClient()
         self.mqtt_client.callback = self.my_callback
         self.mqtt_client.connect("fisherds/to_pi", "fisherds/to_computer", use_off_campus_broker=True)
-
-
 
     def my_callback(self, message_type, message_payload):
         print(f"Type: {message_type}   Payload: {message_payload}")
@@ -30,13 +29,28 @@ class App:
             
         if message_type == "drive":
             print(f"Drive the tank treads at {message_payload}.")
-            # TODO: use the rosebot class
-           
+            self.robot.drive_system.drive(message_payload)
+        
+        if message_type == "line_sensor_stream":
+            if message_payload == "on":
+                self.robot.is_streaming_line_sensors = True
+            if message_payload == "off":
+                self.robot.is_streaming_line_sensors = False
+
+        if message_type == "mode":
+            self.robot.set_mode(message_payload)
+
+
 def main():
     print("Ready")
     app = App()
 
+    last_sensor_send_time = time.time()
     while True:
+        if app.robot.is_streaming_line_sensors and time.time() - last_sensor_send_time > 2:
+            last_sensor_send_time = time.time()
+            app.mqtt_client.send_message("line_sensors", app.robot.line_sensors.get_values())
+        app.robot.update_mode()
         time.sleep(0.1)
   
 
